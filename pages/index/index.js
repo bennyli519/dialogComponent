d202d525ddd39a77eaee6fed3b4a71d891c139e3
jerryli519 <519248932@qq.com>
2018-04-12 20:00:34 +0800

    代码提交

---
+++ pages/index/index.js
@@ -0,0 +1,79 @@
+// pages/book/shared/time/time.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    visible: false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    this.Modal = this.selectComponent("#modal");
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  _onShowModal: function (e) {
+    this.Modal.showModal();
+
+  },
+  _confirmEventFirst: function () {
+    console.log("点击确定了!");
+    this.Modal.hideModal();
+  },
+  _cancelEvent: function () {
+    console.log("点击取消!");
+  }
+
+})
